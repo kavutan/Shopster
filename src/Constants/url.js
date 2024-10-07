9ffcde19cfeb42a32cf30a5f6f885f44d5b42d93
kavutan <kavutan605@gmail.com>
2024-10-07 23:16:38 +0300

    Оновлено рендеринг підкатегорій на використання category.links

--- src/Constants/url.js
+++ src/Constants/url.js
@@ -2,3 +2,5 @@ export const REGISTER_URL = 'http://localhost:5123/gateway/users';
 export const LOGIN_URL = 'http://localhost:5123/gateway/autorizes';
 export const GETMYORDERS_URL = 'http://localhost:5123/gateway/orders';
 export const FORGOT_PASSWORD_URL = 'http://localhost:5123/gateway/forgotpassword';
+export const PERENTCATEGORYCATEGORY = 'http://localhost:5123/gateway/category';
+export const CATEGORY = 'http://localhost:5123/gateway/category';
